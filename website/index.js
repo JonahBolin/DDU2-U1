d57b39d92ead6a5551e748cb3cd0c3fe090896ab
JonahBolin <jonah.bolin17@gmail.com>
2024-11-20 13:44:14 +0100

    Citybuttons
    Skapade knappar (divar) för varje stad  genom funktionen createCityButton och en for-loop

--- website/index.js
+++ website/index.js
@@ -4,3 +4,18 @@
 
 // Recommended: Ask for the city name and then the rest of the code
 
+//Skapa en for-loop som ska gå igenom varje stad i arrayen cities
+//Skapa en funktion som skapar en div för varje button
+
+const bigDivForNewDivs = document.querySelector("#cities");
+function createCityButton(cityName) { //Hur vet programmet att cityname hänvisar till en nyckel i arrayen cities??
+    const newDiv = document.createElement("div");
+    newDiv.classList.add("cityBox");
+    newDiv.textContent = cityName;
+    bigDivForNewDivs.appendChild(newDiv) //Kan man hämta barnet genom dess id/klass??
+}
+
+for (i = 0; i < cities.length; i++) {
+    createCityButton(cities[i].name)
+}
+
